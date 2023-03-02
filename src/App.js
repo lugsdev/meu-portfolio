453531ab8605cc824106f14ba604b39dfb81741e
Lucas Almeida <113302691+lugsdev@users.noreply.github.com>
2023-03-01 22:09:54 -0300

    Criação de Nav Bar e conteúdo do Header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBarMenu from './components/NavBarMenu';
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Banner } from './components/Banner';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBarMenu />
+      <Banner />
+
+      {/* Colocar no Footer
+      <a href="https://storyset.com/technology">Technology illustrations by Storyset</a> */}
     </div>
   );
 }
